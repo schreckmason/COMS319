67fa2a2d3c2e54cde6ef0504d940a8258f511b73
Mason <schreck@iastate.edu>
2017-01-29 12:34:37 -0600

    Fixed multiple image propagation, page now works as it should so far. Need to validate drop down boxes is all now, I emailed Mitra to see what constitutes a pass case for that

--- Lab2/validation1.js
+++ Lab2/validation1.js
@@ -1,29 +1,34 @@
 function validate1(){
     var valCheck=true;
-    var image1=setImage(firstCheck(document.forms["personalInfo"]["firstName"]));
+    var image1=getImage(firstCheck(document.forms["personalInfo"]["firstName"]),"First Name");
     document.getElementById("First Name").appendChild(image1)
    
-   image1=setImage(lastCheck(document.forms["personalInfo"]["lastName"]));
+   image1=getImage(lastCheck(document.forms["personalInfo"]["lastName"]),"Last Name");
    document.getElementById("Last Name").appendChild(image1);
 }
 
+//Function that will return green check if true, red x if otherwisre
+function getImage(bool, ID){
+    var image=document.getElementById("image"+ID);
+    if(image==null){
+        image=new Image(15,15);
+        image.id="image"+ID;
+    }
+    image.src=bool ? './correct.png' : './wrong.png'
+    return image;
+}
+
 function firstCheck(firstName){
-    //only checks for a blank box, input a length check and special character check here
-    if(firstName.value==''){
+    //Don't need to check for blank box, implicit check for that in length check
+    if(firstName.value.length<2){
         return false;
     }
     return true;
 }
 
 function lastCheck(lastName){
-    if(lastName.value==''){
+    if(lastName.value.length<2){
         return false;
     }
     return true;
-}
-
-function setImage(bool){
-    image=new Image(15,15);
-    image.src=bool ? "./correct.png" : './wrong.png'
-    return image;
 }
