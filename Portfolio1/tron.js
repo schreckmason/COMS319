ce9230f57795197c8f716b01ed78669d4cd94f84
Drake <dmossman@iastate.edu>
2017-02-07 12:15:54 -0600

    Made some simple interface functions for tron clients to use

--- Portfolio1/tron.js
+++ Portfolio1/tron.js
@@ -1,31 +1,62 @@
-function toggleCrawl(){
-   if(crawling){
-      clearInterval(timer);
-   } else {
-      timer = setInterval(function() {forward();}, delay);
-   }
-   crawling = !crawling;
-}
+//I want to create some sort of data structure to contain variables about a "tron bike/player"
+//maybe something as simple as object literals (example 2 in https://www.phpied.com/3-ways-to-define-a-javascript-class/)
+//Then client methods would edit one of a series of player objects in an array
 
+// Interface functions to be called when input received from clientInformation
 function right(){
-   direction = (direction + 1) % 4;
+   turn(2);
 }
 
 function left(){
-   direction = (direction + 3) % 4;
+   turn(0);
 }
 
-function forward(){
-   // update position to be crawlLength units in the right direction
-   position[direction%2] += crawlLength * (Math.floor(direction/2)*2 - 1);
-   
-   ctx.lineTo(position[0], position[1]);
-   ctx.stroke();
-   
-   if(!withinBounds()){
-      toggleCrawl();
+function up(){
+   turn(1);
+}
+
+function down(){
+   turn(3);
+}
+
+
+
+// "private"/internal functions
+function turn(int dir){
+   // if not opposite current direction;
+   if(Math.abs(direction - dir) != 2){
+      direction = dir;
    }
 }
+
+// function toggleCrawl(){
+   // if(crawling){
+      // clearInterval(timer);
+   // } else {
+      // timer = setInterval(function() {forward();}, delay);
+   // }
+   // crawling = !crawling;
+// }
+
+// function right(){
+   // direction = (direction + 1) % 4;
+// }
+
+// function left(){
+   // direction = (direction + 3) % 4;
+// }
+
+// function forward(){
+   // update position to be crawlLength units in the right direction
+   // position[direction%2] += crawlLength * (Math.floor(direction/2)*2 - 1);
+   
+   // ctx.lineTo(position[0], position[1]);
+   // ctx.stroke();
+   
+   // if(!withinBounds()){
+      // toggleCrawl();
+   // }
+// }
 
 function init(){
    canvas = document.getElementById("myCanvas");
@@ -43,14 +74,14 @@ function init(){
    ctx.moveTo(position[0], position[1]);
 }
 
-function withinBounds(){
-   if(position[0]>=0 && position[1]>=0 && position[0]<=dims[0] && position[1]<=dims[1]){
+// function withinBounds(){
+   // if(position[0]>=0 && position[1]>=0 && position[0]<=dims[0] && position[1]<=dims[1]){
       // if within bounds
-      return true;
-   } else {
+      // return true;
+   // } else {
       // if outside bounds, bring back to wall
-      position[0] = Math.max(0, Math.min(position[0], dims[0]));
-      position[1] = Math.max(0, Math.min(position[1], dims[1]));
-      return false;
-   }
-}
+      // position[0] = Math.max(0, Math.min(position[0], dims[0]));
+      // position[1] = Math.max(0, Math.min(position[1], dims[1]));
+      // return false;
+   // }
+// }
