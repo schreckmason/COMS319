384ef8cf2278dd98a8c6893c808edb93b416eb7a
Drake <dmossman@iastate.edu>
2017-02-10 20:31:11 -0600

    Got initial animation and control working for two players (use wasd, arrows for turning); still need collision checking

--- Portfolio1/tron.js
+++ Portfolio1/tron.js
@@ -13,6 +13,8 @@ will change the value of the pixels in row 29 and 30 by 50% for alpha and some f
 On the other hand, if the line is width 2, it completely overwrites the data above and below
 For minimal complexity of logic, I'd like to always draw in a way that completely overwrites data and uses full alpha.
 The simplest way to achieve this is even, integral line widths and integral position coordinates.
+
+Could talk about requestAnimationFrame as new/complex
 */
 
 
@@ -20,11 +22,12 @@ The simplest way to achieve this is even, integral line widths and integral posi
 
 /*                                     START OF PLAYER DEFINITION                               */
 // Define Player "class"
-function Player (name) {
-   this.name = "Drake";
-   this.color = 0xFF0000; // red
-   this.direction = 1;// 0:left, 1:up, 2:right, 3:down
-   this.position = [0, 0];
+function Player (name, color, direction, position) {
+   this.name = name;//"Drake";
+   this.color = color;//"#FF0000"; // red
+   this.direction = direction;//1;// 0:left, 1:up, 2:right, 3:down
+   this.position = position;//[0, 0];
+   this.alive = true;
 }
 // Player "public" functions
 Player.prototype.right  = function(){this.turn(2);};
@@ -40,19 +43,62 @@ Player.prototype.getInfo = function(){
 // Player "private"/internal functions
 Player.prototype.turn = function(dir){
    // if not opposite current direction;
-   if(Math.abs(this.direction - dir) != 2){
+   if(Math.abs(this.direction - dir) != 2)
       this.direction = dir;
-   }
+};
+   
+Player.prototype.forward = function(pixels){
+   var dir = this.direction;
+   this.position[dir%2] += pixels * (Math.floor(dir/2)*2 - 1);
 };
 /*                                     END OF PLAYER DEFINITION                               */
-
-
-function test1(){
-   ctx.strokeStyle = "#FF0000";
-   ctx.lineTo(31, 47);
-   ctx.stroke();
+function animationDraw(){
+   //ctx.clearRect(0,0, canvas.width, canvas.height);//fresh slate
+   players.forEach(advancePlayer);
+   raf = window.requestAnimationFrame(animationDraw);
 }
 
+function advancePlayer(player){
+   if(player.alive){
+      var pos0 = [player.position[0], player.position[1]];
+      player.forward(1);
+      checkCollision(player.position);
+      erasePlayer(pos0);
+      ctx.strokeStyle = player.color;
+      console.log(player.getInfo());
+      drawPlayer(player.position);
+      ctx.moveTo(pos0[0], pos0[1]);
+      ctx.lineTo(player.position[0], player.position[1]);
+      ctx.stroke();
+   }
+}
+
+function drawPlayer(position){
+   ctx.fillRect(position[0]-2, position[1]-2, 4, 4);
+}
+function erasePlayer(position){
+   ctx.clearRect(position[0]-2, position[1]-2, 4, 4);
+}
+function checkCollision(position){
+   
+}
+
+function test1(){
+   window.cancelAnimationFrame(raf);
+}
+
+function rightAll(){
+   players.forEach(function(player){player.right();});   
+}
+function leftAll(){
+   players.forEach(function(player){player.left();});
+}
+function upAll(){
+   players.forEach(function(player){player.up();});   
+}
+function downAll(){
+   players.forEach(function(player){player.down();});
+}
 function test21(p1, p2){console.log(p1.getInfo() + '\n' + p2.getInfo());}
 function test2(){
    var player1 = new Player("Drake");
@@ -104,19 +150,49 @@ function test3(){
 // }
 
 function init(){
+   raf = window.requestAnimationFrame(animationDraw);
+   
    canvas = document.getElementById("myCanvas");
    ctx = canvas.getContext("2d");
-   
-   dims = [canvas.width, canvas.height];
-   // crawlLength = 2;
-   delay = 40;
-   crawling = false;
-   
    ctx.lineWidth = 2;
    ctx.strokeStyle = "#FF0000";
-   ctx.moveTo(31,41);
+   
+   players = [];
+   players.push(new Player("Mason", "#FF0000", 0, [canvas.width, canvas.height/2]));
+   players.push(new Player("Drake", "#0000FF", 2, [0, canvas.height/2]));
+   test21(players[0], players[1]);
+   window.addEventListener("keydown", keyPressed, false);
 }
 
+function keyPressed(e) {
+    switch(e.keyCode) {
+        case 37: // left key pressed
+            players[1].left();
+            break;
+        case 38: // up key pressed
+            players[1].up();
+            break;
+        case 39: // right key pressed
+            players[1].right();
+            break;
+        case 40: // down key pressed
+            players[1].down();
+            break;
+        case 87: // w key pressed
+            players[0].up();
+            break;
+        case 65: // a key pressed
+            players[0].left();
+            break;
+        case 83: // s key pressed
+            players[0].down();
+            break;
+        case 68: // d key pressed
+            players[0].right();
+            break; 
+            
+    }   
+}
 // function withinBounds(){
    // if(position[0]>=0 && position[1]>=0 && position[0]<=dims[0] && position[1]<=dims[1]){
       // if within bounds
