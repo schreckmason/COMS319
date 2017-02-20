956240d23149f02c074c191aa7c57a3c2c319968
Drake <dmossman@iastate.edu>
2017-02-20 16:21:25 -0600

    Got many handlers working; Still need to implement and, or, and memory functions, plus EC

--- Lab3/calculatorBinary.js
+++ Lab3/calculatorBinary.js
@@ -1,9 +1,11 @@
 var Calculator = {
     Model : {
-        
+        operand: undefined,
+        operator: undefined,
+        append: true
     },
     View : {
-        resultRow : {id: "resultRow", type: "text", value:"", onclick:""},
+        display : {id: "display", type: "text", value:"", onclick:""},
         button1 : {id: "button1", type: "button", value: 1, onclick: ""},
         button0 : {id: "button0", type: "button", value: 0, onclick: ""},
         inverse : {id: "inverse", type: "button", value: "~", onclick: ""},
@@ -24,11 +26,104 @@ var Calculator = {
         buttonEq : {id: "buttonEq", type: "button", value: "=", onclick: ""}
     }, 
     Controller : {
+        binToDecimal : function(input){
+            var positive = !input.startsWith('-');
+            var charArray = input.slice(positive?0:1).split('');
+            var result =0;
+            for(var i=charArray.length-1; i>=0; i--){
+                result += parseInt(charArray[i])*Math.pow(2,charArray.length-i-1);
+            }
+            return positive? result: -1*result;
+        },
         
+        decToBinary : function(num){
+            bin = [];
+            var positive = num >= 0;
+            num = Math.abs(num);
+            while(num!=0){
+                bin.splice(0,0,num%2);
+                num = Math.floor(num/2);
+            }
+            bin = bin.join("");
+            return positive? bin: '-'+bin;
+        },
+        
+        operand_handler : function(operand){
+            if(Calculator.Model.append){//Calculator.Model.operator === undefined 
+                display.value += operand.value;
+            } else if(!Calculator.Model.append){//Calculator.Model.operator instanceof String 
+                display.value = operand.value;
+                Calculator.Model.append = true;
+            }
+        },
+        
+        unary_operator_handler : function(operator){
+            if(operator.value === '~'){
+                console.log("inverse called");
+                display.value = display.value.replace(/0/g, 'X').replace(/1/g, '0').replace(/X/g, '1');;
+            } else if(operator.value === '>>'){
+                console.log("right shift called");
+                display.value = '0' + display.value.slice(0, -1);
+            } else {
+                //left shift (<<)
+                console.log("left shift called");
+                display.value = display.value.slice(1) + '0';
+            }
+        },
+        
+        binary_operator_handler : function(operator){
+            console.log(Calculator.Model.operand);
+            
+            Calculator.Model.operand = Calculator.Controller.binToDecimal(display.value);
+            
+            console.log(Calculator.Model.operand);
+            
+            Calculator.Model.operator = operator.value;
+            Calculator.Model.append = false;
+        },
+        
+        equals_handler : function(){
+            console.log("equals called");
+            
+            var arg1 = Calculator.Model.operand;
+            var op = Calculator.Model.operator;
+            var arg2 = Calculator.Controller.binToDecimal(display.value);
+            var result;
+            
+            console.log(arg1);
+            console.log(op);
+            console.log(arg2);
+            
+            if(op === '+'){
+                result = Calculator.Controller.decToBinary(arg1+arg2); 
+                
+            } else if(op === '-'){
+                result = Calculator.Controller.decToBinary(arg1-arg2);
+            } else if(op === '*'){
+                result = Calculator.Controller.decToBinary(arg1*arg2); 
+            } else if(op === '/'){
+                result = Calculator.Controller.decToBinary(Math.floor(arg1/arg2)); 
+            } else if(op === '%'){
+                result = Calculator.Controller.decToBinary(arg1%arg2); 
+            } else if(op === '&'){
+                //result has number of digits same as longer operand
+                
+            } else if(op === '|'){
+                //result has number of digits same as longer operand
+                
+            }
+            display.value = result; 
+        },
+        clear_handler : function(){
+            Calculator.Model.operator = undefined;
+            Calculator.Model.operand = undefined;
+            Calculator.Model.append = true;
+            display.value = "";
+        }
     },
     run :function(){
         Calculator.attachHandlers();
-        console.log(Calculator.display());
+        // console.log(Calculator.display());
         return Calculator.display();
     },
     displayElement : function(element) {
@@ -43,7 +138,7 @@ var Calculator = {
     display : function() {
         var s;
         s = "<table id=\"myTable\" border=2>";
-        s += "<tr><td>" + Calculator.displayElement(Calculator.View.resultRow) + "</td></tr>";
+        s += "<tr><td>" + Calculator.displayElement(Calculator.View.display) + "</td></tr>";
         s += "<tr><td>";
         s += Calculator.displayElement(Calculator.View.button1);
         s += Calculator.displayElement(Calculator.View.button0);
@@ -78,22 +173,21 @@ var Calculator = {
         return s;
     },
     attachHandlers : function() {
+        Calculator.View.button1.onclick="Calculator.Controller.operand_handler(this)";
+        Calculator.View.button0.onclick="Calculator.Controller.operand_handler(this)";
         
-    },
-    attachOperator : function() {
+        Calculator.View.buttonEq.onclick="Calculator.Controller.equals_handler(this)";
         
-    },
-    
-    convertToDecimal : function(input){
-        var charArray = input.split('');
-        var result =0;
-        for(var i=charArray.length-1; i>=0; i--){
-            var count=0;
-            if(parseInt(charArray[i]==1)){
-                result += charArray[i]*Math.pow(2,count);
-            }
-            count++;
+        Calculator.View.clear.onclick="Calculator.Controller.clear_handler(this)";
+        
+        var unary = ["inverse", "right", "left"];
+        for(var i=0;i<unary.length;i++){
+            Calculator.View[unary[i]].onclick="Calculator.Controller.unary_operator_handler(this)";
+        }
+        
+        var binary = ["addition", "sub", "mult", "div", "modulus", "and", "or"];
+        for(var i=0;i<binary.length;i++){
+            Calculator.View[binary[i]].onclick="Calculator.Controller.binary_operator_handler(this)";
         }
     }
-
 }
