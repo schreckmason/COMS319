56f59e8b369c16671ccf9087fba3b00adeaa21d6
Drake <dmossman@iastate.edu>
2017-03-08 11:49:11 -0600

    Made sql query for creating tables

---
+++ Lab6/createDatabase.sql
@@ -0,0 +1,39 @@
+drop table Users;
+drop table Book;
+drop table LoanHistory;
+drop table Shelf;
+drop table BookLocation;
+
+create table Users(
+	UserName VARCHAR(255),
+	Password VARCHAR(255),
+	Email VARCHAR(255),
+	Phone INT(10),
+	Librarian TINYINT,
+	FirstName VARCHAR(255),
+	LastName VARCHAR(255),
+    Primary Key (UserName));
+
+create table Books(
+	BookId INT(10),
+	BookTitle VARCHAR(255),
+	Author VARCHAR(255),
+	Availablity TINYINT,
+    Primary Key (BookId));
+
+create table LoanHistory(
+	UserName VARCHAR(255),
+	BookId INT(10),
+	DueDate DATE,
+	ReturnedDate DATE,
+    Primary Key (UserName));
+
+create table Shelves(
+	ShelfId INT(10),
+	ShelfName VARCHAR(255),
+    Primary Key (ShelfId));
+    
+create table BookLocation(
+	BookId INT(10),
+	ShelfId INT(10),
+    Primary Key (BookId));
